69bfd7479a1f3f615a3a429e6c8f4f76223e0531
SeWZC <36623158+SeWZC@users.noreply.github.com>
2025-11-05 11:34:22 +0800

    添加 JavascriptMemberNameAttribute 以设置 JS 下的方法名
    (cherry picked from commit 7976e572de0e2cb413d7b371d34fcebc4608c856)

---
+++ CefGlue.Common/JavascriptMemberNameAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Xilium.CefGlue.Common;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class JavascriptMemberNameAttribute(string name) : Attribute
+{
+    public string Name { get; } = name;
+}
